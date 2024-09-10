398c5d30c069a2c18e456b132045dc5889c5cb12
Vitaliy <vitalij.minecraft@yandex.ru>
2024-09-10 10:17:51 +0500

    Add files via upload

---
+++ labs/lab1/Task2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 4;
+    int c = a + b;
+    
+    printf("c = %d\n", c);
+    
+    int a1 = 65;
+    int b1 = 12;
+    int c1 = a1 + b1;
+    
+    printf("c = %d\n", c1);
+    
+    int a2 = 99;
+    int b2 = 88;
+    int c2 = a2 + b2;
+    
+    printf("c = %d\n", c2);
+    
+    return 0;
+}
